bc9f83c81c09d3c87fce93c0051ac86b90911125
Marcin Martowicz <marcinmartowicz9@gmail.com>
2025-03-04 18:03:33 +0100

    Simplify cam_lang proof

--- CAM/LangIffCAM.v
+++ CAM/LangIffCAM.v
@@ -16,54 +16,43 @@ Theorem cam_lang :
       ~OctxHandlesOp C' l ->
       C[ C'[e_do l w]ₒ ]ᵢ -->* v).
 Proof.
-  intros. remember ⟨v, i_ctx_top⟩ₑ as s'. induction H as [| s s2 s']; subst.
-  - split; intros.
-    + injection H; intros; subst. simpl. apply rt1n_refl.
-    + discriminate.
-  - inversion H; subst; split; intros; try discriminate;
-    injection H1; intros; subst; clear H1;
-    specialize (IHclos_refl_trans_1n eq_refl);
+  intros. remember ⟨v, i_ctx_top⟩ₑ as s'. induction H as [| s s2 s'];
+    subst; intros.
+  - split; try discriminate; intros.
+    injection H; intros; subst. simpl. apply rt1n_refl.
+  - specialize (IHclos_refl_trans_1n eq_refl).
     destruct IHclos_refl_trans_1n as [IH1 IH2].
+    inversion H; subst; split; intros; try discriminate;
+    injection H1; intros; subst; clear H1.
     + eapply rt1n_trans.
       * apply red_context. apply red_add.
       * apply IH1. reflexivity.
     + eapply rt1n_trans.
       * apply red_context. apply red_app.
       * apply IH1. reflexivity.
-    + assert (H' : C0 [e_let e1 e2]ᵢ = (i_ctx_let C0 e2) [e1]ᵢ).
-      { reflexivity. }
-      rewrite H'. apply IH1. reflexivity.
-    + assert (H' : C0 [e_handle e h]ᵢ = (i_ctx_handle C0 h) [e]ᵢ).
-      { reflexivity. }
-      rewrite H'. apply IH1. reflexivity.
-    + simpl. eapply rt1n_trans.
-      * apply red_context. apply red_let.
-      * apply IH1. reflexivity.
-    + simpl. eapply rt1n_trans.
-      * apply red_context. apply red_handle_ret.
-      * apply IH1. reflexivity.
-    + assert (H' : C0 [e_do l v0 ]ᵢ = C0 [o_ctx_hole [e_do l v0 ]ₒ ]ᵢ).
-      { reflexivity. }
-      rewrite H'. apply IH2.
-      * reflexivity.
-      * simpl. auto.
-    + assert (H' : i_ctx_let C e2 [C'0 [e_do l0 w ]ₒ ]ᵢ =
-                   C [(o_ctx_let C'0 e2) [e_do l0 w ]ₒ ]ᵢ).
-      { reflexivity. }
-      rewrite H'. apply IH2.
-      * reflexivity.
-      * simpl. assumption.
-    + assert (H' : i_ctx_handle C h [C'0 [e_do l0 w ]ₒ ]ᵢ =
-                   C [(o_ctx_handle C'0 h) [e_do l0 w ]ₒ ]ᵢ).
-      { reflexivity. }
-      rewrite H'. apply IH2.
-      * reflexivity.
-      * simpl. intro. destruct H1 as [Hcontra | Hcontra]; auto.
+    + specialize (IH1 e1 (i_ctx_let C0 e2) eq_refl).
+      apply IH1.
+    + specialize (IH1 e (i_ctx_handle C0 h) eq_refl).
+      apply IH1.
     + eapply rt1n_trans.
-      * simpl. apply red_context. apply red_handle_do.
-        -- apply HHandlesOp.
-        -- assumption.
+      * simpl. apply red_context. apply red_let.
       * apply IH1. reflexivity.
+    + eapply rt1n_trans.
+      * simpl. apply red_context. apply red_handle_ret.
+      * apply IH1. reflexivity.
+    + specialize (IH2 C0 o_ctx_hole l v0 eq_refl).
+      apply IH2. auto.
+    + specialize (IH2 C (o_ctx_let C'0 e2) l0 w eq_refl).
+      apply IH2. simpl. assumption.
+    + specialize (IH2 C (o_ctx_handle C'0 h) l0 w eq_refl).
+      apply IH2. simpl. tauto.
+    + specialize (IH1 (esubst (esubst e_op (vshift w))
+                      (v_lam (e_handle (o_ctx_shift C'0 [v_var VZ ]ₒ) (hshift h))))
+                  C eq_refl).
+      simpl. eapply rt1n_trans.
+      * apply red_context. apply red_handle_do; try assumption.
+        apply HHandlesOp.
+      * apply IH1.
 Qed.
 
 Ltac inv H := inversion H; subst; clear H.
@@ -87,7 +76,7 @@ Ltac crush_cam_red :=
   | [ |- ⟨ _ +ᵢ _, _, _, _ ⟩ₒ ==>* _ ] => rt1n_trans2; (try apply add_op_mode_cam_red_o)
   end).
 
-Lemma lang_cam_one_step :
+Lemma lang_cam_step:
   forall (V : Set) (C C' : i_ctx V) e r1 r2,
     r1 ~~> r2 ->
     C[e]ᵢ = C'[r1]ᵢ ->
@@ -99,10 +88,10 @@ Proof.
   - apply plug_atomic_eq_plug_i in H0 as [C2 [? ?]]; auto; subst.
     crush_cam_red.
   - apply plug_compound_eq_plug_i with (C' := o_ctx_let o_ctx_hole e0)
-      (v := v) in H0 as [[HC He'] | [C2 [? ?]]]; subst; auto; simpl;
+      (v := v) in H0 as [[HC He'] | [C2 [? ?]]]; auto; subst;
     crush_cam_red.
   - apply plug_compound_eq_plug_i with (C' := o_ctx_handle o_ctx_hole h)
-      (v := v) in H0 as [[HC He'] | [C2 [? ?]]]; subst; auto; simpl;
+      (v := v) in H0 as [[HC He'] | [C2 [? ?]]]; auto; subst;
     crush_cam_red.
   - apply plug_handle_do_eq_plug_i in H0 as
       [[C2 [? ?]] | [C00 [C01 [? [? ?]]]]]; subst.
@@ -121,20 +110,18 @@ Proof.
   induction multi as [| e1 e2]; intros; subst.
   - apply val_eq_plug in H as [H1 H2]. subst. apply rt1n_refl.
   - apply red_decomposition in H as [C' [r1 [r2 [HC [He2 Hr]]]]]. subst e2.
-    pose (lang_cam_one_step _ _ _ _ _ _ Hr HC) as Hred.
+    pose (lang_cam_step _ _ _ _ _ _ Hr HC) as Hred.
     rt1n_trans2.
     + apply Hred.
     + apply IHmulti; auto.
 Qed.
 
-Theorem lang_equiv_cam :
+Theorem lang_iff_cam :
   forall (V : Set) (e : expr V) (v : value V),
     e -->* v <-> ⟨e, i_ctx_top⟩ₑ ==>* ⟨v, i_ctx_top⟩ₑ.
 Proof.
   intros. split; intro.
-  - apply lang_cam with (C := i_ctx_top) (e' := e) in H.
-    + assumption.
-    + simpl. reflexivity.
+  - apply lang_cam with (e := e); auto.
   - apply cam_lang in H as [H1 _].
     apply H1 with (C := i_ctx_top). reflexivity.
 Qed.
